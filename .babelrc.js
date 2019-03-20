e08d618486e26b7f908eed0e3834a0345bea4d21
ygjack <ygjack414@hotmail.com>
2019-03-20 13:39:17 +0800

    update babelrc

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+const ENV = process.env['ENV']
+const presets = ENV === 'test' ? [
+  [
+    "@babel/preset-env",
+    { targets: "> 0.25%, not dead" }
+  ]
+] : []
+
+const plugins = [
+  "@babel/plugin-proposal-class-properties",
+  "@babel/plugin-proposal-object-rest-spread"
+]
+
+module.exports = {
+  presets, plugins
+}
